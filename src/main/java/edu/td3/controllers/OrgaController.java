3afe2dc616aa323d890bcfadeb1c2fd92942fc13
qgorak <q.gorak@gmail.com>
2020-09-24 11:56:42 +0200

    rest constroller

--- src/main/java/edu/td3/controllers/OrgaController.java
+++ src/main/java/edu/td3/controllers/OrgaController.java
@@ -4,8 +4,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import edu.td3.models.Organization;
 import edu.td3.repositories.GroupRepository;
@@ -22,7 +27,7 @@ import io.github.jeemv.springboot.vuejs.VueJS;
 
  
 @Controller
-@RequestMapping("/orgas/")
+@RequestMapping("/rest/")
 public class OrgaController {
 	@Autowired
     private OrgaRepository repo;
@@ -34,12 +39,34 @@ public class OrgaController {
 	@Autowired
 	private VueJS vue;
 	
-	@GetMapping("/")
-	public String index(ModelMap model) {
+	@GetMapping("/orgas/")
+	public String read(ModelMap model) {
+
 	    List<Organization> organizations = repo.findAll();
 		vue.addData("organizations", organizations);
 		model.put("vue", vue);
 		return "index";
 	}
+	
+	@GetMapping("/orgas/{id}")
+	public String read(@PathVariable int id,ModelMap model) {
+		Organization orga = repo.findById(id);
+		vue.addData("orga", orga);
+		model.put("vue", vue);
+		return "index";
+	}
+	
+	@PostMapping("/orgas/create")
+    public void create(@ModelAttribute Organization orga) {
+		Organization e = orga;
+		repo.saveAndFlush(e);
+   
+    }
+	
+	@DeleteMapping("/orgas/delete/{id}")
+    public RedirectView delete(@PathVariable int id) {
+		repo.deleteById(id);
+        return new RedirectView("/orgas/");
+    }
 	
 }
